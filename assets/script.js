7913b92057346a5fa1c3c943670ef1a5c7a9f6b7
Neal Muller <neal.henri.muller@gmail.com>
2025-05-08 10:45:36 +0200

    Ajout du script fonctionnel V1

--- assets/script.js
+++ assets/script.js
@@ -17,14 +17,46 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-/*Ajout des event lisener */
-
+/****** Déclaration des variables ***********/
 const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dotsConteneur = document.querySelector(".dots");
+const nbImage = slides.length;
+const image = document.querySelector(".banner-img");
+let i = 0;
+
+/****** Ajout des bullets points ***********/
+for (let i = 0; i < nbImage; i++) {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dotsConteneur.appendChild(dot);
+}
+//querySelector va chercher par essence le premier élement avec la class .dot
+const dot = document.querySelectorAll(".dot");
+dot[i].classList.add("dot_selected");
+//dot.classList.add("dot_selected");
+
+/****** Ajout des events listener ***********/
 arrowLeft.addEventListener("click", () => {
   console.log("Clique gauche");
 });
 
-const arrowRight = document.querySelector(".arrow_right");
 arrowRight.addEventListener("click", () => {
   console.log("Clique droit");
+  if (i < nbImage - 1) {
+    dot[i].classList.remove("dot_selected");
+    i++;
+    dot[i].classList.add("dot_selected");
+    image.src = `./assets/images/slideshow/${slides[i].image}`;
+  } else {
+    dot[i].classList.remove("dot_selected");
+    i = 0;
+    dot[i].classList.add("dot_selected");
+    image.src = `./assets/images/slideshow/${slides[i].image}`;
+  }
+  console.log(i);
 });
+
+function sliderUpdate(index) {
+  image.src = `./assets/images/slideshow/${slides[index].image}`;
+}
